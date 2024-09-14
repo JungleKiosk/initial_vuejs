baf626684e093ae6bc24180cec6d3c0b9febd800
Fra <fra24eso@gmail.com>
2024-09-15 00:05:26 +0200

    count btn

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,16 +1,21 @@
 <script setup>
 
+import {ref} from 'vue'
+
+const count= ref(0)
+
 </script>
 
 <template>
-  <div class="container">
-    <div class="row">
-      <div class="col-12">
+  <div class="container mt-5">
+    <div class="row justify-content-center">
+      <div class="col-3 text-center">
         <h1>Bootstrap</h1>
+        <button class="btn btn-danger" @click="count++">{{ count }}</button>
       </div>
     </div>
   </div>
-  <h1>Hello Vue!</h1>
+
 
 </template>
 
