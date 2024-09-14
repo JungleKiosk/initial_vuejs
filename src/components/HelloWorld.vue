bc6d2ba75706d11df95a2aed180c213edeeea85e
Fra <fra24eso@gmail.com>
2024-09-14 23:47:50 +0200

    initial commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,19 @@
+<script setup>
+
+</script>
+
+<template>
+  <div class="container">
+    <div class="row">
+      <div class="col-12">
+        <h1>Bootstrap</h1>
+      </div>
+    </div>
+  </div>
+  <h1>Hello Vue!</h1>
+
+</template>
+
+<style scoped>
+
+</style>
